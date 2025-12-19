726d100017b1b4c8055dede9bf0caf938fb9f701
Q <Q24SS@yahoo.com>
2025-12-19 16:13:20 -0500

    HomePage.js

---
+++ HomePage.js
@@ -0,0 +1,15 @@
+Public Dynamic Class Homepage()=> Tuple([str, int,maps]): 
+    
+    Const All_vars = Queue(tuple[Header, Div-Toggle, Div-Menubar,
+         Div-Sidebar, Footer])(index))
+    
+    Const Header = Document.QuerySelector('All_vars[Header]');
+    Const Div-Toggle = Document.QuerySelectorAll('Toggle');
+    Const Div-Sidebar = Document....
+    Const Div-Menubar = Document ....
+    Const Footer = Document....
+    
+    All_Vars.AddEventListener(onclick(), => (Q) { 
+      
+    
+    }
