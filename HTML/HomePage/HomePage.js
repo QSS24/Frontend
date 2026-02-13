3c26b7d7f23232829b84db3b37d802d923791bb0
Q <Q24SS@yahoo.com>
2026-02-13 11:31:48 -0500

    Refactor Homepage class for dynamic elements
    Refactor Homepage class to improve dynamic element handling and event listener setup.

---
+++ HTML/HomePage/HomePage.js
@@ -0,0 +1,30 @@
+Public Dynamic Class Homepage()=> Tuple([str, int,maps]): 
+    
+    Const All_vars = Queue(tuple[Header, Div-Toggle, Div-Menubar,
+         Div-Sidebar, Footer])(index))
+
+
+    Const Dynamic_Elements = Tuples({'Buttons': ' ',
+                    'Div': ' ',
+                    'Forms': ' ',
+                    'Menubar': ' ',
+                    'Sidebar': ' ',
+                    'Scrollbar': ' ',
+                    'Toggle': ' ',
+                    'Nav' : ' ',
+                    'Container': ' ',
+                   })
+    For each _ ,i   in Dynamic_Elements if not [none, bool] :
+        While: 
+            Const _, i(re.compile('\b*Div\'g) = Document.QuerySelector('
+                                  
+    Const Header = Document.QuerySelector('All_vars[Header]');
+    Const Div-Toggle = Document.QuerySelectorAll('Toggle');
+    Const Div-Sidebar = Document....
+    C
+    All_Vars.AddEventListener(onclick(), => (Q) { 
+      
+    
+    }
+
+            
