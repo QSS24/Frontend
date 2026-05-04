c8e7ac42ecf6f6cb116e1d4ef3022a0851fae881
Q <Q24SS@yahoo.com>
2026-05-04 14:06:36 -0400

    Implement Sidebar Toggles functionality

---
+++ HTML/Sidebar Dropdown.js
@@ -0,0 +1,17 @@
+import UseState from 'React'
+
+Function Toggles() {
+    Const Togging = Document.QuerySelectAll('Sidebar-Toggles') => tuples(( str[]SideToggles, str[]: DropDownToggles)) {                                                                                                            
+        New Toggles = Togging.AddEventListener('click', => 
+            { forEach '( i, m, n ) in const New-Togging = Document.QuerySelectAll('Sidebar-Toggles['*']) ' if New-Togging[:] != None:
+                Try: 
+                   
+                Error as Err:
+
+                Finally: 
+                    
+          
+        })
+    )};
+
+}
