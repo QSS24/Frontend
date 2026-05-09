a30221edbc8f123d02749545abfe7ab259a126c0
Q <Q24SS@yahoo.com>
2026-05-09 14:20:16 -0400

    Refactor Sidebar Dropdown.js for better functionality
    Fix syntax errors and improve event listener for sidebar toggles.

--- HTML/Sidebar Dropdown.js
+++ HTML/Sidebar Dropdown.js
@@ -1,11 +1,16 @@
+#!/usr/bin/venv javascript 
+
 import UseState from 'React'
 
 Function Toggles() {
     Const Togging = Document.QuerySelectAll('Sidebar-Toggles') => tuples(( str[]SideToggles, str[]: DropDownToggles)) {                                                                                                            
         New Toggles = Togging.AddEventListener('click', => 
-            { forEach '( i, m, n ) in const New-Togging = Document.QuerySelectAll('Sidebar-Toggles['*']) ' if New-Togging[:] != None:
+            { forEach " ( i, m, n ) in const New-Togging = Document.QuerySelectAll(Sidebar-Toggles['*']) "
                 Try: 
-                   
+                   Return {
+                    <a href = " Toggles.UseState('Previous State') " > 
+                    </a> 
+                    }            
                 Error as Err:
 
                 Finally: 
