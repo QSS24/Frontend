5478ee90eed9428781bad96bb8a6d5ff30a1b56e
Q <Q24SS@yahoo.com>
2026-05-15 13:00:18 -0400

    Add Sidebar-Dropdown.js for toggle functionality

---
+++ JavaScript/Sidebar-Dropdown.js
@@ -0,0 +1,22 @@
+#!/usr/bin/venv javascript 
+
+import UseState from 'React'
+
+Function Toggles() {
+    Const Togging = Document.QuerySelectAll('Sidebar-Toggles') => tuples(( str[]SideToggles, str[]: DropDownToggles)) {                                                                                                            
+        New Toggles = Togging.AddEventListener('click', => 
+            { forEach " ( i, m, n ) in const New-Togging = Document.QuerySelectAll(Sidebar-Toggles['*']) "
+                Try: 
+                   Return {
+                    <a href = " Toggles.UseState('Previous State') " > 
+                    </a> 
+                    }            
+                Error as Err:
+
+                Finally: 
+                    
+          
+        })
+    )};
+
+}
