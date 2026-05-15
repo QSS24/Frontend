0711d6791534ab48a3c5a204133b963011f3e9ea
Q <Q24SS@yahoo.com>
2026-05-15 15:33:12 -0400

    Rename Signup to CreateAccount
    Refactor Signup component into CreateAccount with improved imports and structure.

---
+++ JavaScript/CreateAccount.js
@@ -0,0 +1,25 @@
+From React import 'STATEUSE'
+From React import 'React'
+Import { View, Text, ScrollView, StyleSheet, Flatlist, platform, Button, Pressable } from 'React-Native' 
+Import { UseEffect } from 'React' 
+Import { Touchablehighlight } from 'React-Native' 
+
+
+
+Class Private Dynamic Main CreateAccount( Email: [ str, int ], Members : [ str, int ]):
+    const Create = ( a, i ) => ({
+        'UseEffect, Onclick' (() => {
+            <ScrollView>
+            if(!  <Touchablehighlight> 
+                    <Section id = "create"> 
+                        <form  class = "Create"> 
+                            <lable id = "Create" type= "text> Create Accounts </lable> 
+                            <Pressable onPress = {('UseEffect', )> 
+                
+                                    /* Once Pressed does */ 
+                
+                            </Pressable> 
+                        </form> 
+                </TouchableHighlight> 
+        })
+    })
